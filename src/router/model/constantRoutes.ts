083791342022d1524abd0899405ac1fe5aeef10a
“用户名” <2961198736@qq.com>
2021-06-05 15:34:45 +0800

    登录

--- src/router/model/constantRoutes.ts
+++ src/router/model/constantRoutes.ts
@@ -1,5 +1,5 @@
 import { RouteRecordRaw } from "vue-router";
-import Layout from "../../layout/Index.vue";
+import Layout from "@/layout/Index.vue";
 /**
  * constantRoutes
  * 没有权限要求的基本页面
@@ -13,10 +13,17 @@ export const constantRoutes: Array<RouteRecordRaw> = [
     children: [
       {
         path: "index",
-        component: () => import("../../views/Home.vue"),
+        component: () => import("@/views/Home.vue"),
         name: "index",
         meta: { title: "首页", icon: "el-icon-house", requireAuth: true },
       },
     ],
   },
+  {
+    path: "/login",
+    name: "login",
+    component: () =>  import("@/views/Login.vue"),
+    meta: { requireAuth: false },
+  },
+  
 ];
